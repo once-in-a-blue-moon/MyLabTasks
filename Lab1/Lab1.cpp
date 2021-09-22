dd8eabd1b5d89d6c95fad43dae3aa4cd49d2a423
Валерия Саркисян <vsarkisan62@gmail.com>
2021-09-22 13:01:21 +0300

    change if

--- Lab1/Lab1.cpp
+++ Lab1/Lab1.cpp
@@ -5,7 +5,7 @@ using std::cin;
 
 int main()
 {
-	int r;
+	double r;
 	double pi = 3.1415926535;
 	double a;
 	double b;
@@ -24,24 +24,15 @@ int main()
 	cin >> r;
 	cout << "\n";
 	double n = a + b + c;
-	if (n == 180) {
-		if (r >= 0) {
-			double x = sin(a * pi / 180), y = sin(b * pi / 180), z = sin(c * pi / 180);
-			double stor1, stor2, stor3;
-			stor1 = 2 * x * r;
-			stor2 = 2 * y * r;
-			stor3 = 2 * z * r;
-			cout << "сторона 1 = " << stor1 << "см\n";
-			cout << "сторона 2 = " << stor2 << "см\n";
-			cout << "сторона 3 = " << stor3 << "см\n";
-		}
-		else {
-			int i = 0;
-			while (i < 50) {
-				cout << "ERROR! The user is TOO DUMB! This problem CAN'T be fixed!\n";
-				i++;
-			}
-		}
+	if (n == 180 && r >= 0 && a >= 0 && b >= 0 && c >= 0) {
+		double x = sin(a * pi / 180), y = sin(b * pi / 180), z = sin(c * pi / 180);
+		double stor1, stor2, stor3;
+		stor1 = 2 * x * r;
+		stor2 = 2 * y * r;
+		stor3 = 2 * z * r;
+		cout << "сторона 1 = " << stor1 << "см\n";
+		cout << "сторона 2 = " << stor2 << "см\n";
+		cout << "сторона 3 = " << stor3 << "см\n";
 	}
 	else {
 		int j = 0;
