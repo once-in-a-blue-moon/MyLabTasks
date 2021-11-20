fd4307ff287aef104079176c886c1df99f62d127
Lera <vsarkisan62@gmail.com>
2021-11-20 17:32:15 +0200

    fixed lab 5

--- lab5/lab5.cpp
+++ lab5/lab5.cpp
@@ -6,17 +6,20 @@ int main()
 	cout << "Enter a, b:" << endl;
 	cin >> a >> b;
 	for (int n = a; n <= b; n++){ 
-		int k = 0, S = 0;
-		cout << "n = " << n;
-		for (int i = 1; i <= n; i++) {
-			if (n % i == 0) {
-				k = k + 1;
-				S = S + i;
+		if (n>0){
+			int k = 0, S = 0;
+			cout << "n = " << n;
+			for (int i = 1; i <= n; i++) {
+				if (n % i == 0) {
+					k = k + 1;
+					S = S + i;
 				
-			}
+				}
 		
+			}
+			cout << ". Number of dividers: " << k << "; Sum of dividers: " << S << endl;
 		}
-		cout << ". Number of dividers: " << k << "; Sum of dividers: " << S << endl;
+		
 	}	
 	return 0;
 }
