37edc3bbe9e4233a2bf0f176099a628ac5fca9c7
Lera <vsarkisan62@gmail.com>
2021-11-23 01:06:12 +0200

    lab 6

--- lab6/lab6.cpp
+++ lab6/lab6.cpp
@@ -1,29 +1,31 @@
 ﻿#include <iostream>
 #include <cmath>
+#define _USE_MATH_DEFINES
 using namespace std;
 
-int a, b, n;
+
 const double pi = 3.14159265358979323846;
 
-double integral_1(int);
-double integral_2(int, int, int);
-double result();
+double integral_1(int k);
+double integral_2(int beginning, int end, int k);
+double result(int a, int b, int n);
 
 int main(){
 	cout << "calculate sum of two integrals" << endl;
+	int a, b, n;
 	cout << "Enter a = "; cin >> a;
 	cout << "Enter b = "; cin >> b;
 	cout << "Enter n = "; cin >> n;
 	cout << "integral 1 = " << integral_1(n) << endl; //значення інтеграла від 0 до pi
 	cout << "integral 2 = " << integral_2(a, b, n) << endl; //значення інтеграла від a до b
-	cout << "Result = " << result(); //значення шуканого виразу
+	cout << "Result = " << result(a, b, n); //значення шуканого виразу
 }
 
-double integral_1(int m) {
+double integral_1(int k) {
 	//для знаходження першого інтеграла використаємо подану формулу, де замість a і b буде 0 і pi відповідно
 	double sum = 0;
-	double h = pi / m;
-	for (int i = 1; i <= m; i++) {
+	double h = pi / k;
+	for (int i = 1; i <= k; i++) {
 		double x, f;
 		x = i * h - h / 2; //значення х-ітого на даному кроці
 		f = log(2 + sin(x)); //значення f від х-ітого на даному кроці
@@ -33,17 +35,17 @@ double integral_1(int m) {
 }
 double integral_2(int beginning, int end, int k) {
 	//для знаходження першого інтеграла використаємо подану формулу
-	double sum2 = 0;
-	double h2 = (double)(end - beginning) / k; //знаходження h для формули
-	for (int j = 1; j <= k; j++) {
-		double x2, f2;
-		x2 = beginning + j * h2 - h2 / 2;
-		f2 = pow(atan(x2), 2);
-		sum2 = sum2 + f2;
+	double sum = 0;
+	double h = (float)(end - beginning) / k; //знаходження h для формули
+	for (int i = 1; i <= k; i++) {
+		double x, f;
+		x = beginning + i * h - h / 2;
+		f = pow(atan(x), 2);
+		sum = sum + f;
 	}
-	return (h2 * sum2);
+	return (h * sum);
 }
-double result() {
+double result(int a, int b, int n) {
 	double res = integral_1(n) + integral_2(a, b, n);
 	return res;
 }
