70d83c3eaba09ead6de6c60ce279a98637206e53
Lera <vsarkisan62@gmail.com>
2021-11-24 12:48:58 +0200

    lab 5 (edited)

--- lab5/lab5.cpp
+++ lab5/lab5.cpp
@@ -5,21 +5,25 @@ int main()
 	float a, b;
 	cout << "Enter a, b:" << endl;
 	cin >> a >> b;
-	for (int n = a; n <= b; n++){ 
-		if (n>0){
-			int k = 0, S = 0;
-			cout << "n = " << n << ". dividers are:";
-			for (int i = 1; i <= n; i++) {
-				if (n % i == 0) {
-					k++;
-					S += i;
-					cout << " " << i;
+	if (a > b)
+		cout << "b must be > a";
+	else {
+		for (int n = a; n <= b; n++) {
+			if (n > 0) {
+				int k = 0, S = 0;
+				cout << "n = " << n << ". dividers are:";
+				for (int i = 1; i <= n; i++) {
+					if (n % i == 0) {
+						k++;
+						S += i;
+						cout << " " << i;
+					}
+
 				}
-		
+				cout << ". Number of dividers: " << k << "; Sum of dividers: " << S << endl;
 			}
-			cout << ". Number of dividers: " << k << "; Sum of dividers: " << S << endl;
+
 		}
-		
-	}	
+	}
 	return 0;
 }
